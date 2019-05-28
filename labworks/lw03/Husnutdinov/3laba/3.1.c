a6cbf899bfab29463d2721c3d55b28356b49d86e
subaruman <40674162+subaruman@users.noreply.github.com>
2019-05-28 15:16:33 +0300

    Add files via upload
    3 laba

---
+++ labworks/lw03/Husnutdinov/3laba/3.1.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+    printf ("Лабораторная работа №3.1 \nВыполнил Хуснутдинов А.И ИСТбд-21\n");
+    pid_t pid, ppid; //Тип данных pid_t является синонимом для одного из целочисленных типов языка C
+    /* Узнаем идентификаторы текущего и родительского процесса*/
+    pid = getpid();
+    ppid = getppid();
+    printf("pid = %d, ppid = %d", (int)pid, (int)ppid);
+    return 0;
+}
