3d97236f4110d78b319397b34cb777159f2f32f4
EgorLar <51672735+EgorLar@users.noreply.github.com>
2019-06-10 20:31:08 +0300

    Add files via upload

---
+++ labworks/lw02/Larehin/2.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+int main() {
+    printf ("Лабораторная работа № 2.1\
+            \nЛарёхин Е.Н. Псбд-21 ");
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir("/home/");
+    if (!dir) {
+        perror("diropen");
+        exit(1);
+    };
+
+    while ( (entry = readdir(dir)) != NULL) {
+        printf("%d - %s [%d] %d\n",
+            entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
+    };
+
+    closedir(dir);
+};
